1edc0713d1d15c25add1a71830e3b136163b04c5
debel damilola <debeldamilola@gmail.com>
2021-02-10 15:05:24 +0100

    error validation implemented

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 require('dotenv').config(); //load config variable
 
 const company = require('./routes/company.js'); //import company routes
+const errorHandler = require('./middlewares/errorHandler.js'); //error handler middleware
 
 const connectDB = require('./utils/db.js'); //import database
 
@@ -13,6 +14,7 @@ app.use(express.json()); //body parser
 connectDB(); //connect to database
 
 app.use('/api/company', company);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,36 @@
+const ErrorResponse = require('../utils/error');
+
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+  //   console.log(err);
+
+  //mongoose bad objectID error handling
+  if (err.name === 'CastError') {
+    const message = `Resource with the id ${err.value} not found`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  //mongoose duplicate key error collection:
+  if (err.code === 11000) {
+    const message = `Resource field '${JSON.stringify(err.keyValue).replace(
+      /"/g,
+      "'"
+    )}' already exist`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  //Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  console.log(err.name);
+
+  res
+    .status(error.statusCode || 500)
+    .json({ success: false, error: error.message || 'server error' });
+};
+
+module.exports = errorHandler;
